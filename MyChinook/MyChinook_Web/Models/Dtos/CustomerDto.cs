508e08ef1de44ae15e472655c7b82bb9284cd776
TALENAKLE\DELL <thirakarn.k@gofive.co.th>
2023-03-22 09:37:16 +0700

    update

--- MyChinook/MyChinook_Web/Models/Dtos/CustomerDto.cs
+++ MyChinook/MyChinook_Web/Models/Dtos/CustomerDto.cs
@@ -9,16 +9,16 @@ namespace MyChinook_Web.Models.Dtos
         public string FirstName { get; set; } = null!;
         [Required]
         public string LastName { get; set; } = null!;
-        public string? Company { get; set; }
-        public string? Address { get; set; }
-        public string? City { get; set; }
-        public string? State { get; set; }
-        public string? Country { get; set; }
-        public string? PostalCode { get; set; }
-        public string? Phone { get; set; }
-        public string? Fax { get; set; }
+        public string Company { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Country { get; set; }
+        public string PostalCode { get; set; }
+        public string Phone { get; set; }
+        public string Fax { get; set; }
         [Required]
         public string Email { get; set; } = null!;
-        public int? SupportRepId { get; set; }
+        public int SupportRepId { get; set; }
     }
 }
